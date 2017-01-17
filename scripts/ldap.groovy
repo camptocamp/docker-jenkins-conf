b2af62b18fdc81b37be3a43190c56fc23cd40c3e
Marc Sutter <marc.sutter@swisscom.com>
2017-01-17 12:12:47 +0100

    add ldap settings

---
+++ scripts/ldap.groovy
@@ -0,0 +1,18 @@
+import jenkins.model.*
+import hudson.security.*
+import org.jenkinsci.plugins.*
+
+String server = env['JENKINS_LDAP_SERVER']
+String rootDN = env['JENKINS_LDAP_ROOT_DN']
+String userSearchBase = env['JENKINS_LDAP_USER_SEARCH_BASE']
+String userSearch = env['JENKINS_LDAP_USER_SEARCH_FILTER']
+String groupSearchBase = env['JENKINS_LDAP_GROUP_SEARCH_BASE']
+String groupSearchFilter = env['JENKINS_LDAP_GROUP_SEARCH_FILTER']
+String managerDN = env['JENKINS_LDAP_MANAGER_USER_DN']
+String managerPassword = env['JENKINS_LDAP_MANAGER_USER_PASSWORD']
+boolean inhibitInferRootDN = false
+
+SecurityRealm ldap_realm = new LDAPSecurityRealm(server, rootDN, userSearchBase, userSearch, groupSearchBase, groupSearchFilter, managerDN, managerPassword, inhibitInferRootDN)
+
+Jenkins.instance.setSecurityRealm(ldap_realm)
+Jenkins.instance.save()

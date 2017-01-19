8745390cd47bb2f476c10efa0736756393d2552d
Marc Sutter <marc.sutter@swisscom.com>
2017-01-19 10:20:49 +0100

    fix ldap groovy script

--- scripts/11_ldap.groovy
+++ scripts/11_ldap.groovy
@@ -1,22 +1,24 @@
-import jenkins.model.*
-import hudson.security.*
-import org.jenkinsci.plugins.*
+import jenkins.model.Jenkins;
+import hudson.security.FullControlOnceLoggedInAuthorizationStrategy;
+import hudson.security.LDAPSecurityRealm;
 
 def instance = Jenkins.getInstance()
 def env = System.getenv()
 
-String server = env['JENKINS_LDAP_SERVER']
-String rootDN = env['JENKINS_LDAP_ROOT_DN']
-String userSearchBase = env['JENKINS_LDAP_USER_SEARCH_BASE']
-String userSearch = env['JENKINS_LDAP_USER_SEARCH_FILTER']
-String groupSearchBase = env['JENKINS_LDAP_GROUP_SEARCH_BASE']
-String groupSearchFilter = env['JENKINS_LDAP_GROUP_SEARCH_FILTER']
-String managerDN = env['JENKINS_LDAP_MANAGER_USER_DN']
-String managerPassword = env['JENKINS_LDAP_MANAGER_USER_PASSWORD']
-boolean inhibitInferRootDN = false
+instance.authorizationStrategy = new FullControlOnceLoggedInAuthorizationStrategy() ;
+instance.securityRealm= new LDAPSecurityRealm(
+  server = env['JENKINS_LDAP_SERVER'],
+  rootDN = env['JENKINS_LDAP_ROOT_DN'],
+  userSearchBase = env['JENKINS_LDAP_USER_SEARCH_BASE'],
+  userSearch = env['JENKINS_LDAP_USER_SEARCH_FILTER'],
+  groupSearchBase = env['JENKINS_LDAP_GROUP_SEARCH_BASE'],
+  groupSearchFilter = env['JENKINS_LDAP_GROUP_SEARCH_FILTER'],
+  groupMembershipStrategy = null,
+  managerDN = env['JENKINS_LDAP_MANAGER_USER_DN'],
+  managerPasswordSecret = env['JENKINS_LDAP_MANAGER_USER_PASSWORD'],
+  inhibitInferRootDN = false,
+  disableMailAddressResolver = false,
+  cache = null
+  );
 
-SecurityRealm ldap_realm = new LDAPSecurityRealm(server, rootDN, userSearchBase, userSearch, groupSearchBase, groupSearchFilter, managerDN, managerPassword, inhibitInferRootDN)
-
-instance.setSecurityRealm(ldap_realm)
 instance.save()
-instance.reload()

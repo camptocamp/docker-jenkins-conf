ebedc3a5c12fee4f02937ce5f1f282ccb640b985
Marc Sutter <marc.sutter@swisscom.com>
2017-01-19 08:46:22 +0100

    add trustedcert params by import

--- import-crt.sh
+++ import-crt.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# create a custom keystore
+# create a custom keystore location
 CUSTOM_KEYSTORE=$JENKINS_HOME/.keystore/
 mkdir -p $CUSTOM_KEYSTORE
+# copy system keystore to the custom location
 cp $JAVA_HOME/jre/lib/security/cacerts $CUSTOM_KEYSTORE
 
-# import out certs
+# import certs
 echo "$JENKINS_LDAP_CA_CRT" > ca.crt
-$JAVA_HOME/bin/keytool -keystore $JENKINS_HOME/.keystore/cacerts -import -file ca.crt -keypass changeit -storepass changeit -noprompt
+$JAVA_HOME/bin/keytool -keystore $JENKINS_HOME/.keystore/cacerts -import -trustcacerts -file ca.crt -keypass changeit -storepass changeit -noprompt
 
 exec "$@"

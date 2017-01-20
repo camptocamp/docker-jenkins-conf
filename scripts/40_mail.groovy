a7f599daf387166eb7b375c7e51256b243bb48a5
Marc Sutter <marc.sutter@swisscom.com>
2017-01-20 10:43:32 +0100

    fix typo

--- scripts/40_mail.groovy
+++ scripts/40_mail.groovy
@@ -7,7 +7,7 @@ def desc = inst.getDescriptor("hudson.tasks.Mailer")
 desc.setSmtpAuth(env['JENKINS_MAIL_USER'], env['JENKINS_MAIL_PASSWORD'])
 desc.setReplyToAddress(env['JENKINS_MAIL_REPLY'])
 desc.setSmtpHost(env['JENKINS_MAIL_SMTP_HOST'])
-desc.setUseSsl(env['JENKINS_MAIL_SMTP_SSL']toBoolean())
+desc.setUseSsl(env['JENKINS_MAIL_SMTP_SSL'].toBoolean())
 desc.setSmtpPort(env['JENKINS_MAIL_SMTP_PORT'].toString())
 desc.setCharset("UTF-8")
 

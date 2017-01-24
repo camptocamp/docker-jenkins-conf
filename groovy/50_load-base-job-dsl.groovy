2ca954908400154da638cfc631171cded3bc8cc7
Marc Sutter <marc.sutter@swisscom.com>
2017-01-24 13:46:02 +0100

    add groovy base script

--- groovy/50_load-base-job-dsl.groovy
+++ groovy/50_load-base-job-dsl.groovy
@@ -2,8 +2,12 @@
 import javaposse.jobdsl.dsl.DslScriptLoader
 import javaposse.jobdsl.plugin.JenkinsJobManagement
 
-def jobDslScript = new File('base-job-dsl.groovy')
-def workspace = new File('.')
-def jobManagement = new JenkinsJobManagement(System.out, [:], workspace)
+def env = System.getenv()
 
-new DslScriptLoader(jobManagement).runScript(jobDslScript.text)
+if ( Jenkins.instance.pluginManager.activePlugins.find { it.shortName == "job-dsl" } != null ){
+  def jobDslScript = new File("${env('JENKINS_HOME')}/groovy/base-job-dsl.groovy")
+  def workspace = new File('.')
+  def jobManagement = new JenkinsJobManagement(System.out, [:], workspace)
+
+  new DslScriptLoader(jobManagement).runScript(jobDslScript.text)
+}

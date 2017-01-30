fc9f82bb7909b95fa1b778db96bf7be2c6546802
Marc Sutter <marc.sutter@swisscom.com>
2017-01-30 17:13:12 +0100

    add build permissions to teams

--- job-dsl-scripts/base-job.groovy
+++ job-dsl-scripts/base-job.groovy
@@ -4,11 +4,13 @@ def github_user = env['JENKINS_GITHUB_USER']
 def github_org = env['JENKINS_GITHUB_ORG']
 def github_base_job_dsl_repo = env['JENKINS_BASE_JOB_DSL_REPO']
 def jenkins_admin = env['JENKINS_ADMIN_GROUPNAME']
+def teams = env['JENKINS_TEAMS']
 
 def github_cred_id = "${github_user}-token"
 
-def job_name = github_org + '-base-job'
+def job_name = "Auto-Generate Pipelines"
 def github_repo = github_org + '/' + github_base_job_dsl_repo
+
 
 job(job_name) {
     logRotator(-1, 10)
@@ -22,6 +24,12 @@ job(job_name) {
     }
     authorization {
         permissionAll(jenkins_admin)
+        teams.eachLine { line ->
+            def team_params  = line.split(';')
+            def team_ldap_group = team_params[0]
+            permission('hudson.model.Item.Build', team_ldap_group)
+            permission('hudson.model.Item.Read', team_ldap_group)
+        }
     }
     steps {
         dsl {

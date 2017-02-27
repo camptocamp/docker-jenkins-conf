a7408ae304e483929788474d14db21d9cd7ab768
Marc Sutter <marc.sutter@swisscom.com>
2017-02-27 10:05:58 +0100

    test if var present before config

--- init.groovy.d/01_location.groovy
+++ init.groovy.d/01_location.groovy
@@ -3,9 +3,11 @@ import jenkins.model.*
 
 def inst = Jenkins.getInstance()
 def env = System.getenv()
+def jenkins_mail = env['JENKINS_MAIL_ADDRESS']
+def jenkins_url = env['JENKINS_ROOT_URL']
 
 // Set the administrator email address
 def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
-jenkinsLocationConfiguration.setAdminAddress(env['JENKINS_MAIL_ADDRESS'])
-jenkinsLocationConfiguration.setUrl(env['JENKINS_ROOT_URL'])
+if (jenkins_url) { jenkinsLocationConfiguration.setUrl(jenkins_url) }
+if (jenkins_mail) { jenkinsLocationConfiguration.setAdminAddress(jenkins_mail) }
 jenkinsLocationConfiguration.save()
--- init.groovy.d/17_credentials.groovy
+++ init.groovy.d/17_credentials.groovy
@@ -11,47 +11,47 @@ import com.cloudbees.plugins.credentials.domains.*;
 import hudson.util.Secret
 
 def env = System.getenv()
-
 def user = env['JENKINS_GITHUB_USER']
-def description = "Github token for ${user}"
 def token = env['JENKINS_GITHUB_TOKEN']
 
-// add admin credential for github plugin (will be system only)
-def textCredId = "${user}-token-secret-text"
-Credentials textc = (Credentials) new StringCredentialsImpl(
-  CredentialsScope.SYSTEM,
-  textCredId,
-  description,
-  Secret.fromString(token)
-)
+if (user && token) {
 
-println "Add admin text credentials"
-SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), textc)
+  def description = "Github token for ${user}"
+  // add admin credential for github plugin (will be system only)
+  def textCredId = "${user}-token-secret-text"
+  Credentials textc = (Credentials) new StringCredentialsImpl(
+    CredentialsScope.SYSTEM,
+    textCredId,
+    description,
+    Secret.fromString(token)
+  )
 
-// add admin credential for pipeline generation (will be scoped in admin folder)
-def pwCredId = "${user}-token"
-Credentials pwc = (Credentials) new UsernamePasswordCredentialsImpl(
-  CredentialsScope.GLOBAL,
-  pwCredId,
-  description,
-  user,
-  token
-)
+  println "Add admin text credentials"
+  SystemCredentialsProvider.getInstance().getStore().addCredentials(Domain.global(), textc)
 
-jenkins = Jenkins.instance
-for (folder in jenkins.getAllItems(Folder.class)) {
-  if(folder.name.equals('admin')){
-	AbstractFolder<?> folderAbs = AbstractFolder.class.cast(folder)
-    FolderCredentialsProperty property = folderAbs.getProperties().get(FolderCredentialsProperty.class)
-    if(property) {
-        println "Add admin credentials"
-        property.getStore().addCredentials(Domain.global(), pwc)
-    } else {
-        println "Initialize Folder Credentials store and add credentials in global store"
-        property = new FolderCredentialsProperty([pwc])
-        folderAbs.addProperty(property)
+  // add admin credential for pipeline generation (will be scoped in admin folder)
+  def pwCredId = "${user}-token"
+  Credentials pwc = (Credentials) new UsernamePasswordCredentialsImpl(
+    CredentialsScope.GLOBAL,
+    pwCredId,
+    description,
+    user,
+    token
+  )
+
+  jenkins = Jenkins.instance
+  for (folder in jenkins.getAllItems(Folder.class)) {
+    if(folder.name.equals('admin')){
+  	AbstractFolder<?> folderAbs = AbstractFolder.class.cast(folder)
+      FolderCredentialsProperty property = folderAbs.getProperties().get(FolderCredentialsProperty.class)
+      if(property) {
+          println "Add admin credentials"
+          property.getStore().addCredentials(Domain.global(), pwc)
+      } else {
+          println "Initialize Folder Credentials store and add credentials in global store"
+          property = new FolderCredentialsProperty([pwc])
+          folderAbs.addProperty(property)
+      }
     }
   }
 }
-
-

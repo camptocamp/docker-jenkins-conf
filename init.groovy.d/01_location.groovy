e8867d9452d801c383c23fb6335409f607ac6cc8
Marc Sutter <marc.sutter@swisscom.com>
2017-03-03 16:45:25 +0100

    add JENKINS_ROOT_URL

--- init.groovy.d/01_location.groovy
+++ init.groovy.d/01_location.groovy
@@ -1,5 +1,8 @@
 #!groovy
 import jenkins.model.*
+import java.lang.System
+import hudson.model.*
+import jenkins.model.*
 
 def inst = Jenkins.getInstance()
 def env = System.getenv()
@@ -8,6 +11,17 @@ def jenkins_url = env['JENKINS_ROOT_URL']
 
 // Set the administrator email address
 def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
-if (jenkins_url) { jenkinsLocationConfiguration.setUrl(jenkins_url) }
+if (jenkins_url) {
+  // set url
+  println "Set root URL to '${jenkins_url}'"
+  jenkinsLocationConfiguration.setUrl(jenkins_url)
+}
 if (jenkins_mail) { jenkinsLocationConfiguration.setAdminAddress(jenkins_mail) }
 jenkinsLocationConfiguration.save()
+inst.save()
+
+systemMessage = "This Jenkins instance is generated from code.\n " +
+                "Avoid any manual changes since they will be discarded with new deployment.\n "
+
+println "Set system message to:\n ${systemMessage}"
+Jenkins.instance.setSystemMessage(systemMessage)

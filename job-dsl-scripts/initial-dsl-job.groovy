6d7060f69bcbb3a47e06ad4d36a72bcf5a8ca44f
Marc Sutter <marc.sutter@swisscom.com>
2017-03-14 16:04:35 +0100

    add env check

--- job-dsl-scripts/initial-dsl-job.groovy
+++ job-dsl-scripts/initial-dsl-job.groovy
@@ -5,29 +5,31 @@ def github_org = env['JENKINS_GITHUB_ORG']
 def github_base_job_dsl_repo = env['JENKINS_INITIAL_DSL_REPO']
 def jenkins_admin = env['JENKINS_ADMIN_GROUPNAME']
 
-def github_cred_id = "${github_user}-admin-token"
+if (github_user && github_org && github_base_job_dsl_repo && jenkins_admin) {
 
-def folder_name = "admin"
-def job_name = "00_initial_dsl_job"
-def github_repo = github_org + '/' + github_base_job_dsl_repo
+    def github_cred_id = "${github_user}-admin-token"
+    def folder_name = "admin"
+    def job_name = "00_initial_dsl_job"
+    def github_repo = github_org + '/' + github_base_job_dsl_repo
 
-job("${folder_name}/${job_name}") {
-    logRotator(-1, 10)
-    scm {
-        git {
-            remote {
-                github(github_repo)
-                credentials(github_cred_id)
+        job("${folder_name}/${job_name}") {
+            logRotator(-1, 10)
+            scm {
+                git {
+                    remote {
+                        github(github_repo)
+                        credentials(github_cred_id)
+                    }
+                }
+            }
+            authorization {
+                permissionAll(jenkins_admin)
+            }
+            steps {
+                dsl {
+                    external('*.groovy')
+                    removeAction('DELETE')
+                }
             }
         }
-    }
-    authorization {
-        permissionAll(jenkins_admin)
-    }
-    steps {
-        dsl {
-            external('*.groovy')
-            removeAction('DELETE')
-        }
-    }
 }

6b96f80be349bc13a8540d89745160c144c0286b
Marc Sutter <marc.sutter@swisscom.com>
2017-03-21 09:46:17 +0100

    add config repo

--- job-dsl-scripts/initial-dsl-job.groovy
+++ job-dsl-scripts/initial-dsl-job.groovy
@@ -1,30 +1,46 @@
 def env = System.getenv()
 
+def github_initial_dsl_repo_url = env['JENKINS_INITIAL_DSL_REPO']
+def github_initial_dsl_repo_tag = env['JENKINS_INITIAL_DSL_REF']
+
 def github_user = env['JENKINS_GITHUB_USER']
 def github_org = env['JENKINS_GITHUB_ORG']
-def github_base_job_dsl_repo = env['JENKINS_INITIAL_DSL_REPO']
+def github_config_repo = env['JENKINS_GITHUB_CONFIG_REPO']
 def jenkins_admin = env['JENKINS_ADMIN_GROUPNAME']
 
-if (github_user && github_org && github_base_job_dsl_repo && jenkins_admin) {
+if (github_user && github_org && github_config_repo && jenkins_admin) {
 
     def github_cred_id = "${github_user}-admin-user-pw-token"
     def folder_name = "admin"
     def job_name = "00_initial_dsl_job"
-    def github_repo = github_org + '/' + github_base_job_dsl_repo
+    def github_repo = github_org + '/' + github_config_repo
 
-        job("${folder_name}/${job_name}") {
+        pipelinejob("${folder_name}/${job_name}") {
             logRotator(-1, 10)
-            scm {
-                git {
-                    remote {
-                        github(github_repo)
-                        credentials(github_cred_id)
+            dir('config'){
+                scm {
+                    git {
+                        remote {
+                            github(github_repo)
+                            credentials(github_cred_id)
+                        }
                     }
                 }
             }
+
+            scm {
+                git {
+                    remote {
+                        url(github_initial_dsl_repo_url)
+                        refspec(github_initial_dsl_repo_tag)
+                    }
+                }
+            }
+
             authorization {
                 permissionAll(jenkins_admin)
             }
+
             steps {
                 dsl {
                     external('*.groovy')
